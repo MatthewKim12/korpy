0c8d7902d86d63034fd311ae0c1f465953d32928
MatthewKim12 <kimdjn12@gmail.com>
2025-09-01 08:43:53 -0700

    ratio 100%

--- examples/ratio_100%.py
+++ examples/ratio_100%.py
@@ -4,5 +4,5 @@ print(korpy.fully_korean("마인크래프트minecraft")) # False
 
 # presets
 print(korpy.PERCENTAGE) # %.f%%
-print(korpy.PERCENTAGE_UNTIL_1) # %.1%%
-print(korpy.PERCENTAGE_UNTIL_2) # %.2%%
+print(korpy.PERCENTAGE_UNTIL_1) # %.1f%%
+print(korpy.PERCENTAGE_UNTIL_2) # %.2f%%
