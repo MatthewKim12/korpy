c6bd647dc3aaa6eb5b2ed73c9a51edf1a879acac
MatthewKim12 <kimdjn12@gmail.com>
2025-09-07 08:29:06 -0700

    Delete example directory

--- example/is_similar_words.py
+++
@@ -1,8 +0,0 @@
-import korpy
-
-def is_similar_words(word1: str, word2: str) -> bool:
-  return korpy.similarity(word1, word2)>0.8
-
-# test
-print(is_similar_words('한니발', '하이라이스')) # True
-print(is_similar_words('다밭다', '잡두리')) # False
