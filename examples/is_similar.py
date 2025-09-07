7d396a3a57805cbc5c67e7fcd84f909fc9edb5db
MatthewKim12 <kimdjn12@gmail.com>
2025-09-07 08:29:52 -0700

    similar?

---
+++ examples/is_similar.py
@@ -0,0 +1,8 @@
+import korpy
+
+def is_similar(word1: str, word2: str) -> bool:
+  return korpy.similarity(word1, word2)>0.8
+
+# test
+print(is_similar('한니발', '하이라이스')) # True
+print(is_similar('다밭다', '잡두리')) # False
